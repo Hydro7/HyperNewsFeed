e4e1d9d94afe8ed90532335afe5ef330a76baa88
andrewlalis <andrewlalisofficial@gmail.com>
2018-06-09 15:47:41 +0200

    Created resource endpoint newsposts to get all posts.

--- source/backend/app.py
+++ source/backend/app.py
@@ -1,5 +1,23 @@
 from flask import Flask
+from flask_restful import Resource, Api
+from source.backend.database.news_database import NewsDatabase
+
 app = Flask(__name__)
+api = Api(app)
+
+db = NewsDatabase('database/news_database.db')
+
+
+class NewsPosts(Resource):
+    def get(self):
+        posts = db.retrieve_posts()
+        post_json = []
+        for post in posts:
+            post_json.append(post.to_json())
+        return post_json
+
+
+api.add_resource(NewsPosts, '/newsposts')
 
 
 @app.route('/')
---
+++ source/backend/endpoints/news_posts.py
@@ -0,0 +1,6 @@
+
+from flask_restful import Resource, Api
+
+class NewsPosts(Resource):
+
+    def get(self):
