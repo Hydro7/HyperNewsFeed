154001f866ef4192b93b5f7b1c4c995ffcad4961
Hydro7 <said.faroghi@gmail.com>
2018-06-09 20:23:08 +0400

    change

--- source/backend/app.py
+++ source/backend/app.py
@@ -3,15 +3,12 @@ from flask_restful import Api, Resource
 
 from database.news_database import NewsDatabase
 from endpoints.news_posts_resource import NewsPosts
-from database.news_post import NewsPost
-from datetime import datetime
 
 app = Flask(__name__)
 api = Api(app)
 
 db = NewsDatabase('database/news_database.db')
-test_post = NewsPost('test_title', 'test_content', datetime.now(), 'test_company', 'test_address')
-db.store_news_post(test_post)
+
 
 class NewsPosts(Resource):
 
