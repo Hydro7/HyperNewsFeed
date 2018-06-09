f440a05d0ebb9f58b7547098f80d00a540e0293d
Zino Holwerda <z.holwerda@ho2o.nl>
2018-06-09 21:09:07 +0200

    Added titles

--- source/backend/crawler/crawler.py
+++ source/backend/crawler/crawler.py
@@ -27,17 +27,36 @@ def article_crawler(url, company, address, icon_url):
         res = urllib.request.urlopen(url)
         soup = BeautifulSoup(res.read(), "html.parser")
         max1 = 0
-        paragraph = 0
+        par = 0
         for a in soup.findAll('article'):
             for p in a.findAll('p'):
                 size = len(p.text)
                 if size > max1:
                     max1 = size
-                    paragraph = p.text
-        if paragraph != 0:
-            np = NewsPost('blah', paragraph, datetime.now(), company, address, icon_url)
+                    par = p
+        if par != 0:
+            title = par.parent.parent.find('h2').text
+            cnt = 0
+            reached = 0
+            temp_title = ""
+            for c in title:
+                if cnt > 3 and reached != 0:
+                    break
+                if ord(c) == 9 or ord(c) == 13:
+                    continue
+                if c == " ":
+                    if cnt == 0:
+                        temp_title = temp_title + " "
+                    cnt += 1
+                else:
+                    temp_title = temp_title + c
+                    reached = 1
+                    cnt = 0
+            if not title:
+                temp_title = "NULL"
+            print(temp_title)
+            np = NewsPost(temp_title, par.text, datetime.now(), company, address, icon_url)
             db.store_news_post(np)
             print('Saved news post.')
     except:
         pass
-
